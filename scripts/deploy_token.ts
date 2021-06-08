5f212475acb6c9ef17b7b32ebdaec47b94e0badf
wschwab <w.s.schwab4@gmail.com>
2021-06-08 16:05:25 +0300

    initial prototype scripts

---
+++ scripts/deploy_token.ts
@@ -0,0 +1,124 @@
+import hre, { ethers } from "hardhat";
+import "@nomiclabs/hardhat-etherscan";
+import chalk from "chalk";
+import fs from "fs";
+import { Contract } from "ethers";
+import cliProgress from "cli-progress";
+
+interface DeploymentObject {
+  name: string;
+  address: string;
+  args: any;
+  contract: Contract;
+}
+
+// custom `deploy` in order to make verifying easier
+const deploy = async (contractName: string, _args: any[] = [], overrides = {}, libraries = {}) => {
+  console.log(` 🛰  Deploying: ${contractName}`);
+
+  const contractArgs: any = _args || [];
+  const stringifiedArgs = JSON.stringify(contractArgs);
+  const contractArtifacts = await ethers.getContractFactory(contractName,{libraries: libraries});
+  const contract = await contractArtifacts.deploy(...contractArgs, overrides);
+  const contractAddress = contract.address;
+  fs.writeFileSync(`artifacts/${contractName}.address`, contractAddress);
+  fs.writeFileSync(`artifacts/${contractName}.args`, stringifiedArgs);
+
+  // tslint:disable-next-line: no-console
+  console.log("Deploying", chalk.cyan(contractName), "contract to", chalk.magenta(contractAddress));
+
+  await contract.deployed();
+
+  const deployed: DeploymentObject = { name: contractName, address: contractAddress, args: contractArgs, contract };
+
+  return deployed
+}
+
+const verifiableNetwork = ["mainnet", "ropsten", "rinkeby", "goerli", "kovan"];
+
+async function main() {
+  const network = process.env.HARDHAT_NETWORK === undefined ? "localhost" : process.env.HARDHAT_NETWORK;
+  
+  // tslint:disable-next-line: no-console
+  console.log("🚀 Deploying to", chalk.magenta(network), "!");
+  if(
+    network === "localhost" || 
+    network === "hardhat"
+  ) {
+    const [deployer] = await ethers.getSigners();
+
+    // tslint:disable-next-line: no-console
+    console.log(
+      chalk.cyan("deploying contracts with the account:"),
+      chalk.green(deployer.address)
+    );
+
+    // tslint:disable-next-line: no-console
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  }
+
+  let contracts: DeploymentObject[] = [];
+
+  const VITA_CAP = ethers.utils.parseUnits("64298880");
+  const vita =  await deploy("VITA", ["Vita token", "VITA", VITA_CAP]);
+  contracts.push(vita);
+
+  // verification
+  if(
+    verifiableNetwork.includes(network)
+    ) {
+      let counter = 0;
+      
+      // tslint:disable-next-line: no-console
+      console.log("Beginning Etherscan verification process...\n", 
+        chalk.yellow(`WARNING: The process will wait two minutes for Etherscan 
+        to update their backend before commencing, please wait and do not stop 
+        the terminal process...`)
+      );
+
+      const progressBar = new cliProgress.SingleBar({
+        format: 'CLI Progress |' + chalk.cyan('{bar}') + '| { percentage}%',
+        barCompleteChar: '\u2588',
+        barIncompleteChar: '\u2591',
+        hideCursor: true
+      }, cliProgress.Presets.shades_classic);
+      progressBar.start(120, 0);
+
+      const timer = setInterval(() => {
+        counter++;
+        progressBar.update(counter);
+        if(counter >= progressBar.getTotal()){
+          clearInterval(timer);
+          progressBar.stop();
+          // onComplete.apply(this);
+        }
+      }, 1000);
+
+      // tslint:disable-next-line: no-console
+      console.log(chalk.cyan("\n🔍 Running Etherscan verification..."));
+      
+      await Promise.all(contracts.map(async contract => {
+        // tslint:disable-next-line: no-console
+        console.log(`Verifying ${contract.name}...`);
+        try {
+          await hre.run("verify:verify", {
+            address: contract.address,
+            constructorArguments: contract.args
+          });
+          // tslint:disable-next-line: no-console
+          console.log(chalk.cyan(`✅ ${contract.name} verified!`));
+        } catch (error) {
+          // tslint:disable-next-line: no-console
+          console.log(error);
+        }
+      }));
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    // tslint:disable-next-line: no-console
+    console.error(error);
+    process.exit(1);
+  });
